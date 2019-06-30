0159cba8cbef713c861aeb305d1f42d4c86c868a
readme916 <1290144599@qq.com>
2019-06-30 18:45:51 +0800

    1

--- src/layout/header-aside/components/menu-header/index.vue
+++ src/layout/header-aside/components/menu-header/index.vue
@@ -8,7 +8,7 @@
             <d2-layout-header-aside-menu-sub v-else :menu="menu" :key="menuIndex"/>
           </template>
 
-          <li class="el-menu-item" v-if="roleName=='developer'"><a href="http://tree.tienchain.com" target="_blank">社区节点</a></li>
+          <li class="el-menu-item" v-if="roleName=='developer' || roleName=='founder'"><a href="http://tree.tienchain.com" target="_blank">社区节点</a></li>
         </el-menu>
       </div>
     </div>
